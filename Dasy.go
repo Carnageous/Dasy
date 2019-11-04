2ab979df2d18606ab293c3d12da9512fa9ea54c1
Janis Jansen <janis.jansen@gmx.de>
2019-11-04 08:15:21 +0100

    Added client and server websocket handlers

--- Dasy.go
+++ Dasy.go
@@ -2,9 +2,11 @@ package Dasy
 
 import (
 	"strconv"
-	"fmt"
 	"log"
-	"errors"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -54,12 +56,14 @@ func serverHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s Server) Start() error {
-	fmt.Println("Server started on port " + strconv.Itoa(s.Port))
-
+func (s Server) Start() {
+	
 	http.HandleFunc("/echo", serverHandler)
-
-	return nil 	
+	
+	port := strconv.Itoa(s.Port)
+	
+	log.Println("Server started on port " + port)
+	log.Fatal(http.ListenAndServe("localhost:" + port, nil))
 }
 
 /* Client functions */
@@ -70,5 +74,61 @@ func CreateClient() Client {
 }
 
 func (c Client) ConnectToServer(address string) error {
-	return errors.New("Could not connect to server " + address)
+	// Needs to be rewritten using WS over HTTP (not TCP, obviously)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: address, Path: "/echo"}
+	log.Printf("connecting to %s", u.String())
+
+	wc, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer wc.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := wc.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return nil
+		case t := <-ticker.C:
+			err := wc.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return nil
+			}
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := wc.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return nil
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return nil
+		}
+	}
 }
